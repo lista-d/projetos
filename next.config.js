77b575daa03ded181bffbc2f7670d3c83512ce21
lista-d <lista-d@gmail.com>
2025-01-07 23:03:29 +0000

    Refactor project configuration for deployment: updated assetPrefix and basePath in next.config.js, renamed project in package.json and package-lock.json, enhanced TypeScript and Tailwind CSS configurations, and modified GitHub Actions workflow to include CSS build step.

--- next.config.js
+++ next.config.js
@@ -4,9 +4,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/projetos/' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/projetos' : '',
   basePath: process.env.NODE_ENV === 'production' ? '/projetos' : '',
   trailingSlash: true,
+  distDir: 'out',
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 }
 
 module.exports = nextConfig 
